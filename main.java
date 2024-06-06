62c024776ba5500d08c24ad824820fea50051b4b
PRIYADARSHAN2004 <priyadarshan09112004@gmail.com>
2024-06-06 05:04:10 +0000

    first scan

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("hi");
+        int n=sc.nextInt();
+        System.out.print(n);
+
+        sc.close();
+
+    }
+}
