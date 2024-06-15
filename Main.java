ca7f72fa7756dcf8d8b9f75cbab4806cf83e1ba4
PRIYADARSHAN2004 <priyadarshan09112004@gmail.com>
2024-06-15 08:17:13 +0000

    added a comments

--- Main.java
+++ Main.java
@@ -1,38 +1,53 @@
-
 import java.time.LocalTime;
 import java.util.*;
 
 public class Main {
 
+    // 2D array to store user credentials of restaurant managers
     static String userPass[][] = new String[3][2];
+    
+    // 2D array to store the list of dishes and their prices
     static Object listDish[][] = new Object[10][2];
+    
+    // 3D array to store table orders: 5 tables, each with up to 10 dishes, each dish with 3 attributes (name, price, time ordered)
     static Object table[][][] = new Object[5][10][3];
+    
+    // Variables to keep track of the total number of dishes added, total sales, and total dishes delivered
     static int totalDishadded = 0;
     static int Totalsell = 0;
     static int TotalDishdelivered = 0;
-    static Calendar timer= Calendar.getInstance();
+    
+    // Calendar instance for tracking time
+    static Calendar timer = Calendar.getInstance();
+    
     public static void main(String[] args) {
+        // Start the home page
         homepg();
     }
 
     static void homepg() {
-
+        // Initialize default user credentials
         userPass[0][0] = "123";
         userPass[0][1] = "123";
+        
         Scanner sc = new Scanner(System.in);
         int Continuerun = 0;
+        
+        // Home page loop
         while (Continuerun == 0) {
-            System.out.println("~~~~~~~~~~ Restaurant Management ~~~~~~~~~~");//   home page
-            System.out.println("1.Login");
-            System.out.println("2.Signup");
-            System.out.println("0.Exit");
-            int choice=0;
+            System.out.println("~~~~~~~~~~ Restaurant Management ~~~~~~~~~~");
+            System.out.println("1. Login");
+            System.out.println("2. Signup");
+            System.out.println("0. Exit");
+            
+            int choice = 0;
             try {
-                 choice = sc.nextInt();
+                choice = sc.nextInt();
             } catch (Exception e) {
                 System.out.println("````` ERROR: Enter available menu number only `````");
-                homepg();
+                homepg(); // Restart home page in case of invalid input
             }
+            
             System.out.println();
             switch (choice) {
                 case 1:
@@ -49,7 +64,7 @@ public class Main {
                 default:
                     System.out.println();
                     System.out.println();
-                    System.out.println("````` Enter correct memu again `````");
+                    System.out.println("````` Enter correct menu again `````");
                     System.out.println();
                     break;
             }
@@ -59,12 +74,15 @@ public class Main {
     static void LoginPg() {
         Scanner sc = new Scanner(System.in);
         int flag = 1;
+        
+        // Login page loop
         while (flag == 1) {
-
             System.out.print("USERNAME : ");
             String username = sc.next();
             System.out.print("PASSWORD : ");
             String password = sc.next();
+            
+            // Check user credentials
             for (int i = 0; i < userPass.length; i++) {
                 if (username.equals(userPass[i][0]) && password.equals(userPass[i][1])) {
                     System.out.println();
@@ -74,17 +92,18 @@ public class Main {
                     return;
                 }
             }
+            
             System.out.println();
-            System.out.println("````` Entered wrong username password `````");
+            System.out.println("````` Entered wrong username/password `````");
             System.out.println();
-            System.out.println("1.Re-login");
-            System.out.println("2.Home page");
+            System.out.println("1. Re-login");
+            System.out.println("2. Home page");
             System.out.println();
+            
             flag = sc.nextInt();
             if (flag == 2) {
                 return;
             }
-
         }
     }
 
@@ -92,11 +111,14 @@ public class Main {
         System.out.println();
         System.out.println("~~~~~ Signup account ~~~~~");
         System.out.println();
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("USERNAME : ");
         String username = sc.next();
         System.out.print("PASSWORD : ");
         String password = sc.next();
+        
+        // Register new user
         for (int i = 0; i < userPass.length; i++) {
             if (userPass[i][0] == null) {
                 userPass[i][0] = username;
@@ -110,29 +132,33 @@ public class Main {
                 return;
             }
         }
-        System.out.println("Number of admin account are full");
+        
+        System.out.println("Number of admin accounts are full");
         System.out.println();
         System.out.println();
-
     }
 
     static void MainMenu() {
         Scanner sc = new Scanner(System.in);
         int Continuerun = 1;
+        
+        // Main menu loop
         while (Continuerun == 1) {
             System.out.println();
-            System.out.println("1.List of dish today available");
-            System.out.println("2.Add today's dish");
-            System.out.println("3.Bill");
-            System.out.println("4.Analyse");
-            System.out.println("0.Logout");
+            System.out.println("1. List of dishes available today");
+            System.out.println("2. Add today's dish");
+            System.out.println("3. Bill");
+            System.out.println("4. Analyse");
+            System.out.println("0. Logout");
+            
             int choice = 0;
             try {
                 choice = sc.nextInt();
             } catch (Exception e) {
                 System.out.println("````` ERROR: Enter available menu number only `````");
-                MainMenu();
+                MainMenu(); // Restart main menu in case of invalid input
             }
+            
             System.out.println();
             System.out.println();
             switch (choice) {
@@ -155,7 +181,7 @@ public class Main {
                 case 2:
                     System.out.println("~~~~~ Add dish to menu ~~~~~");
                     System.out.println();
-                    System.out.print("Number of dish to add : ");
+                    System.out.print("Number of dishes to add : ");
                     int Tdish = sc.nextInt();
                     System.out.println();
                     Tdish += totalDishadded;
@@ -165,7 +191,6 @@ public class Main {
                                 int sno = i + 1;
                                 if (j == 0) {
                                     System.out.println("Dish name " + sno + ": ");
-
                                     listDish[i][j] = sc.next();
                                 } else {
                                     Integer inputNumber = null;
@@ -193,12 +218,12 @@ public class Main {
                             }
                         }
                         System.out.println();
-                        System.out.println("`````` Dish are added to menu `````");
+                        System.out.println("`````` Dishes are added to menu `````");
                         System.out.println();
                         totalDishadded += Tdish;
                     } else {
                         System.err.println();
-                        System.out.println("````` Entered number is greater than total dishes can added to menu `````");
+                        System.out.println("````` Entered number is greater than total dishes that can be added to menu `````");
                         System.err.println();
                     }
                     break;
@@ -208,12 +233,11 @@ public class Main {
                 case 4:
                     System.out.println("Total sell : " + Totalsell);
                     System.out.println("Total dish delivered : " + TotalDishdelivered);
-
                     break;
                 case 0:
                     return;
                 default:
-                    System.err.println("````` Enter correct memu again `````");
+                    System.err.println("````` Enter correct menu again `````");
                     System.out.println();
             }
         }
@@ -221,8 +245,9 @@ public class Main {
 
     static void bill() {
         Scanner sc = new Scanner(System.in);
-
         int Continuerun = 1;
+        
+        // Billing process loop
         while (Continuerun == 1) {
             System.out.println();
             System.out.println("Select Table number (0-4):");
@@ -231,9 +256,10 @@ public class Main {
             try {
                 TableNumber = sc.nextInt();
             } catch (Exception e) {
-                System.out.println("````` ERROR: Enter available table number only (or) 0 for go back `````");
-                bill();
+                System.out.println("````` ERROR: Enter available table number only (or) 0 to go back `````");
+                bill(); // Restart billing process in case of invalid input
             }
+            
             System.out.println();
             System.out.println();
             if (TableNumber < 0 || TableNumber > 4) {
@@ -248,25 +274,25 @@ public class Main {
             int Continuerun1 = 1;
             while (Continuerun1 == 1) {
                 System.out.println();
-                System.out.println("1.Add dish");
-                System.out.println("2.Orded dish");
-                System.out.println("3.Cancel dish");
-                System.out.println("4.Print bill");
-                System.out.println("0.Back");
+                System.out.println("1. Add dish");
+                System.out.println("2. Ordered dish");
+                System.out.println("3. Cancel dish");
+                System.out.println("4. Print bill");
+                System.out.println("0. Back");
                 int choice = sc.nextInt();
                 switch (choice) {
                     case 1:
-                        System.out.println("List of dish available");
+                        System.out.println("List of dishes available");
                         for (int i = 0; i < totalDishadded; i++) {
                             System.out.println((i + 1) + ". " + listDish[i][0] + " " + listDish[i][1]);
                         }
-                        System.out.print("Enter number of dish to order : ");
+                        System.out.print("Enter number of dishes to order : ");
                         int Totaldishorderintable = sc.nextInt();
                         for (int i = 0; i < Totaldishorderintable; i++) {
                             System.out.println("Enter dish number : ");
                             int DishNum = sc.nextInt();
                             DishNum--;
-                            while (DishNum > totalDishadded) {
+                            while (DishNum >= totalDishadded) {
                                 System.out.println("````` Enter correct dish number in list `````");
                                 DishNum = sc.nextInt();
                                 DishNum--;
@@ -274,10 +300,9 @@ public class Main {
                             table[TableNumber][i][0] = listDish[DishNum][0];
                             table[TableNumber][i][1] = listDish[DishNum][1];
                             LocalTime currentTime = LocalTime.now();
-                            LocalTime futureTime = currentTime.plusSeconds(45);
+                            LocalTime futureTime = currentTime.plusSeconds(45); // Dish can be cancelled within 45 seconds
                             table[TableNumber][i][2] = futureTime;
                         }
-
                         break;
                     case 2:
                         System.out.println();
@@ -288,19 +313,17 @@ public class Main {
                         }
                         break;
                     case 3:
-                    
                         int dishorder = 0;
                         int count = 0;
-                        System.out.println("List of dish orded : ");
-                            for (int i = 0; i < 10; i++) {
-                                if (table[TableNumber][i][1] != null) {
-                                    System.out.println((i + 1) + ". " + table[TableNumber][i][0] + " " + table[TableNumber][i][1]);
-                                    dishorder++;
-                                }
+                        System.out.println("List of ordered dishes : ");
+                        for (int i = 0; i < 10; i++) {
+                            if (table[TableNumber][i][1] != null) {
+                                System.out.println((i + 1) + ". " + table[TableNumber][i][0] + " " + table[TableNumber][i][1]);
+                                dishorder++;
                             }
+                        }
                         System.out.println("~~~ Dish to cancel ~~~");
                         while (count <= dishorder) {
-                            
                             System.out.println();
                             System.out.println("Enter dish number : ");
                             System.out.println("Enter 0 to go back");
@@ -316,24 +339,22 @@ public class Main {
                             System.out.println("currentTimeAfterWait=" + currentTimeAfterWait);
 
                             if (currentTimeAfterWait.isAfter(storedFutureTime)) {
-                                System.out.println("````` You cannot cancel this dish because dish is orded before 30 seconds `````");
+                                System.out.println("````` You cannot cancel this dish because it was ordered more than 45 seconds ago `````");
                             } else {
-                                System.out.println("Orded "+table[TableNumber][DishNumtocancel][0]+" is canceled");
+                                System.out.println("Ordered " + table[TableNumber][DishNumtocancel][0] + " is canceled");
                                 table[TableNumber][DishNumtocancel][0] = null;
                                 table[TableNumber][DishNumtocancel][1] = null;
                                 table[TableNumber][DishNumtocancel][2] = null;
-
                             }
                             count++;
-                            System.out.println("List of dish orded : ");
+                            System.out.println("List of ordered dishes : ");
                             for (int i = 0; i < 10; i++) {
                                 if (table[TableNumber][i][1] != null) {
                                     System.out.println((i + 1) + ". " + table[TableNumber][i][0] + " " + table[TableNumber][i][1]);
-                                    
                                 }
                             }
-
-                        }break;
+                        }
+                        break;
                     case 4:
                         int totalprice = 0;
                         System.out.println();
@@ -344,23 +365,26 @@ public class Main {
                                 System.out.println((i + 1) + ". " + table[TableNumber][i][0] + " " + table[TableNumber][i][1]);
                             }
                             totalprice = table[TableNumber][i][1] != null ? totalprice + ((int) table[TableNumber][i][1]) : totalprice;
-
                         }
 
-                        System.out.println("Total dish : " + Totaldishorder);
+                        System.out.println("Total dishes : " + Totaldishorder);
                         System.out.println("Total price : " + totalprice);
+                        
+                        // Clear table after billing
                         for (int i = 0; i < 10; i++) {
-                            table[TableNumber][i][0] = null;//clear value
-                            table[TableNumber][i][1] = null;//clear value
+                            table[TableNumber][i][0] = null;
+                            table[TableNumber][i][1] = null;
+                            table[TableNumber][i][2] = null;
                         }
-                        Totalsell = Totalsell + totalprice;
-                        TotalDishdelivered = TotalDishdelivered + Totaldishorder;
+                        
+                        Totalsell += totalprice;
+                        TotalDishdelivered += Totaldishorder;
                         break;
                     case 0:
                         Continuerun1++;
                         break;
                     default:
-                        System.err.println("````` Enter correct memu again `````");
+                        System.err.println("````` Enter correct menu again `````");
                         System.out.println();
                 }
             }
